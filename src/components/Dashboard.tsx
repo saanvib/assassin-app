ebb109118e27318e40542dff32a25fd03d3f020a
Saanvi Bhargava <saanvi.bhargava@gmail.com>
2025-03-05 10:05:03 -0800

    show register kill only on target existing

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -122,7 +122,7 @@ function Dashboard() {
         )}
 
         <br></br>
-        {status == "alive" || status == "pending" ? (
+        {target != "" && (status == "alive" || status == "pending") ? (
           <button className="button" onClick={handleOpenKillModal}>
             Register Kill
           </button>
