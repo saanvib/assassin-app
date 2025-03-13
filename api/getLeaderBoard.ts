589921123d7bb02ca7c6d51dabb0ee838f037f90
Saanvi Bhargava <saanvi.bhargava@gmail.com>
2025-03-12 19:38:09 -0700

    Update getLeaderBoard.ts

--- api/getLeaderBoard.ts
+++ api/getLeaderBoard.ts
@@ -9,12 +9,11 @@ export default async function GET(req: VercelRequest, res: VercelResponse) {
    let studentAliveCount = 0;
    for (const item in configItems) {
       const student = configItems[item] as unknown as Student;
-      if (student.status != "eliminated") {
+      if (student.status == "pending") {
          studentAliveCount += 1;
          if (student.killCount >= 1) {
             const { username, killCount } = student;
             studentList.push({ "username": username, "killCount": killCount });
-            studentAliveCount += 1;
          }
 
       }
